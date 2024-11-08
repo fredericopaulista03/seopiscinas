cbf03b21673a11a13275ae8db68e95f2d87b2e2b
fredericopaulista <fredericopaulista@gmail.com>
2024-11-08 17:33:29 -0300

    link rodapé

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,14 +3,14 @@ import { Waves } from 'lucide-react';
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white py-12">
-      <div className="container mx-auto px-4 text-center">
+    <footer className="py-12 text-white bg-gray-900">
+      <div className="container px-4 mx-auto text-center">
         <div className="flex items-center justify-center gap-2 mb-4">
           <Waves className="w-6 h-6" />
           <span className="text-lg font-bold">Consultor SEO Piscinas</span>
         </div>
         <p className="text-gray-400">
-          © {new Date().getFullYear()} Frederico Moura - Especialista em SEO para Empresas de Piscinas
+          © {new Date().getFullYear()} <a href="https://wa.me/5531998955912" title="Frederico Moura" target="_blank">Frederico Moura</a> - Especialista em SEO para Empresas de Piscinas
         </p>
       </div>
     </footer>
